718006d9aca1191a52a58e55379e0ef717fce50e
rutulamin <rutulamin1998@gmail.com>
2022-05-21 16:18:51 +0530

    feat(craw-review): handle pagination

--- modules/crawl/crawlUtils.js
+++ modules/crawl/crawlUtils.js
@@ -14,6 +14,16 @@ crawlUtils.reviews = async (reviewPageURL) => {
             errorMsg: "Review page URL is not valid!"
         };
     }
+    const allReviewData = await crawlUtils.getReviewPageByPage(page, []);
+    await browser.close();
+    return {
+        status: true,
+        data: allReviewData || []
+    };
+};
+
+
+crawlUtils.getReviewPageByPage = async (page, initialData = []) => {
     const reviewBlockSelctor = 'div#CustomerReviewsBlock > div#customerReviews > div#customerReviews';
     const reviewData = await page.$$eval(reviewBlockSelctor, (reviewBlocksElement) => {
         const formattedReviewData = reviewBlocksElement.map(element => {
@@ -29,11 +39,23 @@ crawlUtils.reviews = async (reviewPageURL) => {
         });
         return formattedReviewData;
     });
-    await browser.close();
-    return {
-        status: true,
-        data: reviewData
-    };
+    // Check Next page link exist or not
+    const paginationLinks = await page.$$('dl.reviewPage > dd > a');
+    if (paginationLinks && paginationLinks.length > 0) {
+        const getLastLinkEle = paginationLinks[paginationLinks.length - 1];
+        const getLastLinkText = await getLastLinkEle.evaluate(element => element.innerHTML);
+        const isNextPageExist = getLastLinkText.includes('Next');
+        if (isNextPageExist) {
+            await getLastLinkEle.evaluate(link => link.click());
+            await page.waitForNavigation();
+            const totalReviewData = await crawlUtils.getReviewPageByPage(page, [...initialData, ...reviewData]);
+            return totalReviewData;
+        } else {
+            return [...initialData, ...reviewData];
+        }
+    } else {
+        return [...initialData, ...reviewData];
+    }
 };
 
 module.exports = crawlUtils;
